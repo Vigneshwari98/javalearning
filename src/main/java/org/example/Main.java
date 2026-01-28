c57cddce52ba9f76a16913fcf862e6e359992685
Viki <eshwarieshu98@gmail.com>
2026-01-28 13:34:04 +0530

    Day 1: modify output

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("My name is Vigneshwari");
-        System.out.println("I am Learning Java");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("Day 1 completed successfully");
     }
 }
