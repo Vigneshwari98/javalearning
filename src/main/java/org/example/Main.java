af94e20d0e8b269905aa737f9701ef3a3e5f4253
Viki <eshwarieshu98@gmail.com>
2026-01-29 15:07:30 +0530

    Day 2: Variables, data types and input handling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Welcome to Java Programming");
-        System.out.println("Day 1 completed successfully");
+
+        int age = 14;
+        double height = 141.1;
+        String grade = "A";
+        boolean isLearningJava = false;
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println("Are you learning java " + isLearningJava);
+        double price = 99.99;
+        int number = (int) price;
+        System.out.println(number);
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int ages = scanner.nextInt();
+
+        System.out.println( "My name is " + name + " and my age is " + ages);
+        // float f =100.3;
     }
 }
